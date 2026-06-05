71aa92ff5d44a502e80c9ca194dd99bf4f2a2c4b
gailgirlie-cmd <yanelisabonisani.work@gmail.com>
2026-06-05 09:09:00 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/guidance")
+def guidance():
+    return render_template("guidance.html")
+
+@app.route("/reflection")
+def reflection():
+    return render_template("reflection.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
